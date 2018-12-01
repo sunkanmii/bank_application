e9b3cb1acfa1207754178a0d0e40f28dd6bb6e1b
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2018-12-01 19:56:55 +0100

    banking_application_11

--- my_Script.js
+++ my_Script.js
@@ -1,7 +1,15 @@
 let my_Header = document.getElementById('myHeader');
 let bars = document.getElementById('myBars');
+let close_button = document.getElementById('closeButton');
 
 bars.addEventListener('click', function(){
     my_Header.style.display = 'inline-block';
     bars.style.display = 'none';
+    close_button.style.display = 'inline';
+});
+
+close_button.addEventListener('click', function(){
+    my_Header.style.display = 'none';
+    bars.style.display = 'inline';
+    close_button.style.display = 'none';
 });

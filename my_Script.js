35a17eeade6dea3e73e542db05fb340cd3fc3626
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2018-12-02 20:17:50 +0100

    banking_application_12

--- my_Script.js
+++ my_Script.js
@@ -1,6 +1,8 @@
 let my_Header = document.getElementById('myHeader');
 let bars = document.getElementById('myBars');
 let close_button = document.getElementById('closeButton');
+let login_button = document.getElementById('loginButton');
+let form_close_button = document.getElementById('formCloseButton');
 
 bars.addEventListener('click', function(){
     my_Header.style.display = 'inline-block';
@@ -12,4 +14,12 @@ close_button.addEventListener('click', function(){
     my_Header.style.display = 'none';
     bars.style.display = 'inline';
     close_button.style.display = 'none';
+});
+
+login_button.addEventListener('click', function(){
+    modal_image.style.display = 'inline-block';
+});
+
+form_close_button.addEventListener('click', function(){
+    modal_image.style.display = 'none';
 });
